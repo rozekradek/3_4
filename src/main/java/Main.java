d95e97d853c7c014fbe884009f79b2395fd65fe5
rozekradek <radek2003138@gmail.com>
2025-04-09 11:26:56 +0000

    3_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,42 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   Scanner scanner  = new Scanner(System.in);
+    System.out.println("Wprowadz liczbe a: ");
+    int a = scanner.nextInt();
+    System.out.println("Wprowadz liczbe b: ");
+    int b = scanner.nextInt();
+    System.out.println("Wprowadz liczbe c: ");
+    int c = scanner.nextInt();
+
+    System.out.println("1.Sprawdzanie czy trojkat jest prostokatny");
+    System.out.println("2.Liczenie pola trojkata");
+
+    int wybor=scanner.nextInt();
+
+    switch(wybor)
+      {
+        case 1:
+          if(a*a+b*b==c*c)
+          {
+            System.out.println("Trojkat jest prostokatny");
+          }
+          else
+          {
+            System.out.println("Trojkat nie jest prostokatny");
+          }
+          break;
+        case 2:
+          int p=(a+b+c)/2;
+          double pole=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+          System.out.println("Pole trojkata wynosi: "+pole);
+      }
+
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+
 }
